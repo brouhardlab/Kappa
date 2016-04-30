949e116f01b3419166837f3303f80a957488a0a2
Hadrien Mary <hadrien.mary@gmail.com>
2016-04-30 16:42:29 -0400

    Add BezierPoint

---
+++ src/main/java/fiji/plugin/kappa/curve/BezierPoint.java
@@ -0,0 +1,65 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Fiji.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package fiji.plugin.kappa.curve;
+
+/**
+ *
+ * @author Hadrien Mary <hadrien.mary@gmail.com>
+ */
+public class BezierPoint extends Point2D {
+
+    private static final long serialVersionUID = 1L;
+
+    // Curvature values for a given point along a bezier curve
+    protected double k;
+    protected int sign;
+
+    /**
+     * Constructs a Bezier Point object
+     *
+     * @param x	The x-coordinate of the point
+     * @param y	The y-coordinate of the point
+     * @param k	The curvature of the point
+     */
+    public BezierPoint(double x, double y, double k) {
+        super(x, y);
+        if (k >= 0) {
+            this.sign = 1;
+        } else {
+            this.sign = -1;
+        }
+        this.k = Math.abs(k);
+    }
+
+    /**
+     * Returns a String representing this Bezier Point. This contains its x-coordinate, y-coordinate
+     * and curvature value
+     *
+     * @return	The string representing the Bezier Point
+     */
+    @Override
+    public String toString() {
+        return String.format("x = %f, y = %f, k = %f", x, y, k);
+    }
+}
