6506ba1b0e4d0261408e818d7dfbe71730f93f90
Hadrien Mary <hadrien.mary@gmail.com>
2017-11-17 13:34:59 -0500

    Change copyright owner to Gary Brouhard

--- src/main/java/sc/fiji/kappa/gui/KappaFrame.java
+++ src/main/java/sc/fiji/kappa/gui/KappaFrame.java
@@ -2,7 +2,7 @@
  * #%L
  * A Fiji plugin for Curvature Analysis.
  * %%
- * Copyright (C) 2016 - 2017 Fiji developers.
+ * Copyright (C) 2016 - 2017 Gary Brouhard
  * %%
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
