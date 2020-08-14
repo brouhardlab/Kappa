822fd58bfd81f0b0db76981811c21089952a9cf4
Hadrien Mary <hadrien.mary@gmail.com>
2020-08-14 19:10:51 -0400

    licenses

--- src/test/java/TestManualPlugin.java
+++ src/test/java/TestManualPlugin.java
@@ -3,7 +3,7 @@
  * #%L
  * A Fiji plugin for Curvature Analysis.
  * %%
- * Copyright (C) 2016 - 2017 Gary Brouhard
+ * Copyright (C) 2016 - 2020 Gary Brouhard
  * %%
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
